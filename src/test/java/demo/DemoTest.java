ea6ec0ab8c2176521801ce17b05e28b22e3f1ecf
HP <HP@DESKTOP-D783K9I>
2023-05-03 18:09:55 +0530

    commit 3 - maven project workspace

--- src/test/java/demo/DemoTest.java
+++ src/test/java/demo/DemoTest.java
@@ -4,9 +4,11 @@ import org.testng.annotations.Test;
 
 public class DemoTest {
 	@Test
-	public void ddt()
+	public void ddt ()
 	{
 		System.out.println("this is demo framework........");
+		
+		System.out.println(" hi this is maven project workspace....");
 	}
 
 }
