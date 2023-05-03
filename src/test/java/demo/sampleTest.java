717c1582236e23a2af15c0f12bd7c3b2e847502a
HP <HP@DESKTOP-D783K9I>
2023-05-03 16:11:45 +0530

    commit 1 - DemoTest

---
+++ src/test/java/demo/sampleTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class sampleTest {
+	@Test
+	public void test()
+	{
+		System.out.println("hi this is sample test.........");
+	}
+
+}
